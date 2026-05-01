a226713a0a77eedd17e043cd377a1b6b7112e5f1
akivamac <akivamac@k4r.org>
2026-05-01 09:08:12 -0400

    much bigger detailed responses, brain version 5

--- js/brain.js
+++ js/brain.js
@@ -1,5 +1,5 @@
 const Brain = (() => {
-  const BRAIN_VERSION = '4'; // bump when brain JSON files change
+  const BRAIN_VERSION = '5'; // bump when brain JSON files change
 
   let knowledge = null;
   let rules = null;
