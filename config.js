f15250aa3f8880ffef290520390c0d676a08d7a2
akivamac <akivamac@k4r.org>
2026-05-05 16:26:39 -0400

    feat: GitHub push from Joe + fix regex crash

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-  password: 'x@wf4uZ@UsAig@R9iEJGAYq32Dt',
-  backsideKey: 'bsk_live_KV09cqjWjJQpS2WauaOyANyKVCnntSxR8M6EkT5s9VB',
-  githubToken: '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'
-};
