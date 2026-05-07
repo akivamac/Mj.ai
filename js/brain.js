375b43a9b34f5bff16122a7c6f07a5103482c62d
akivamac <akivamac@k4r.org>
2026-05-07 14:26:21 -0400

    fix: mj v35 rephrase detection + spreadsheet capability

--- js/brain.js
+++ js/brain.js
@@ -1,5 +1,5 @@
 const Brain = (() => {
-  const BRAIN_VERSION = '31'; // bump when brain JSON files change
+  const BRAIN_VERSION = '33'; // bump when brain JSON files change
 
   let knowledge = null;
   let rules = null;
